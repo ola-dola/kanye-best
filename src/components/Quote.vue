f0c2ceaa9744401e396b015a88009ae5027ef59f
ola-dola <oredolaolamide@gmail.com>
2021-08-16 11:11:21 +0100

    implement adding new favourites

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <p>{{ quote }} <button>Add to Fav</button></p>
+    <p>
+      {{ quote.content }}
+      <button @click="addToFav">Add to Fav</button>
+    </p>
   </div>
 </template>
 
@@ -8,5 +11,23 @@
 export default {
   name: "Quote",
   props: ["quote"],
+  methods: {
+    addToFav() {
+      const clientDb = JSON.parse(localStorage.getItem("myFavQuotes"));
+
+      if (!clientDb) {
+        // Initialize new storage
+        return localStorage.setItem(
+          "myFavQuotes",
+          JSON.stringify([this.quote])
+        );
+      }
+
+      localStorage.setItem(
+        "myFavQuotes",
+        JSON.stringify([...clientDb, this.quote])
+      );
+    },
+  },
 };
 </script>
--- src/components/QuoteList.vue
+++ src/components/QuoteList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-bind:key="quote._id" v-for="quote in quotes">
-      <Quote v-bind:quote="quote.content" />
+      <Quote v-bind:quote="quote" />
     </div>
   </div>
 </template>
